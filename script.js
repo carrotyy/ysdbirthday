8c6e73c53341465d0ef304c9c2aeeb990a9bc607
carroty <hanmunkruiser@gmail.com>
2026-07-21 18:18:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // --- 1. SISTEM COUNTDOWN & LAYAR ---
-const targetDate = new Date('August 18, 2026 00:00:00').getTime();
+const targetDate = new Date('August 18, 2025 00:00:00').getTime();
 
 const layarHitam = document.getElementById('layar-hitam');
 const layarBiru = document.getElementById('layar-biru');
