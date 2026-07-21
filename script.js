ef69ccc4bab9282262413a6819516462a30b9440
carroty <hanmunkruiser@gmail.com>
2026-07-21 18:29:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ class Birthday {
     ctx.globalCompositeOperation = 'lighter';
     for (let firework of this.fireworks) firework.update(delta);
 
-    this.counter += delta * 3; 
+    this.counter += delta * 10; 
     if (this.counter >= 1) {
       this.fireworks.push(new Firework(
         random(this.spawnA, this.spawnB),
